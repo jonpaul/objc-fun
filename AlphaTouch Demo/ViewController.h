70d2ab09e37a88844493f21b993727bde929054c
Jon-Paul Lussier Jr <jon.lussier@gfi.com>
2013-11-02 17:24:12 -0400

    Initial Commit

---
+++ AlphaTouch Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlphaTouch Demo
+//
+//  Created by Jon-Paul Lussier on 11/2/13.
+//  Copyright (c) 2013 Jon-Paul Lussier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AlphaTouch Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlphaTouch Demo
+//
+//  Created by Jon-Paul Lussier on 11/2/13.
+//  Copyright (c) 2013 Jon-Paul Lussier. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
